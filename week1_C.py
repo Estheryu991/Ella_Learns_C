ddb516db55f7326fee311ee44fac046ed5f3cc58
Esther Hoeun Yu <togom11@g.skku.edu>
2022-01-25 17:08:35 +0900

    Create week1_C.py

---
+++ week1_C.py
@@ -0,0 +1,10 @@
+C, a general computer programming language, was first used in 1972 and has become one of the most widely studied fields in terms of computer science. One of the basic things that one needs to know is to add “” on the top of their codes to use the command “printf”.
+
+Stdio.h is the shortened version of the word Standard input output header file, which allows one to use the built-in function of printf, which stands for print formatted. There are also other header files such as ,, , and . The C program begins from the command “int main()” so, one should use this after he or she includes the files that one needs.
+
+One should then use curly brackets, which are used to indicate the start of the function. Many computer experts may be familiar with the term “get_string(“”)”. This means that the words inside the two quotation marks are answer codes in terms of the program “scratch”. If one wants to prove that two variables are the same, he or she is obliged to use functions that are usually different in each language. A function is simply a few statements together that finish a task.
+
+One can also use the command “help50” in the terminal to figure out the problems in one’s source code.
+
+https://school.elice.io/courses/13214/lecturerooms/20286/join
+  
