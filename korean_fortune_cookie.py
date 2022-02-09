63540805dad1af97eb43dc451fef4b9313ef6576
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-09 11:48:48 +0100

    Create korean_fortune_cookie.py

---
+++ korean_fortune_cookie.py
@@ -0,0 +1,125 @@
+print("Welcome to Lucky Korean Fortune Game")
+02
+print (" How many bubbles would you like to buy?")
+03
+x = input ()
+04
+valx = int (x)
+05
+if (cookies > 5):
+06
+   print ("\nSorry you do not qualify for the Lucky Korean Fortune Game")
+07
+print (" you only purchased {} cookies.".format(valx))
+08
+print (" For only '$'0 and on more cookie you qualify to play,")
+09
+print ("Would you like to buy more cookies? Enter YES or NO")
+10
+if answer == 'NO':
+11
+   elif:
+12
+     print("Thank you for eating at Lucky's Korean!")
+13
+if answer == 'YES':
+14
+   else:
+15
+     print"\nHow many more cookies would you like to buy?"
+16
+       m = input ()
+17
+       valm = int (m)
+18
+ 
+19
+print ("Welcome to the Lucky Korean Fortune Game!")
+20
+def getfortune():
+21
+    
+22
+    fortuneRand = random.randrange(5) + 1
+23
+     
+24
+    if fortuneRand == 1:
+25
+      print "\nYou will receive a large some of money"
+26
+     
+27
+    elif fortuneRand == 2:
+28
+       print "\nSomething you lost will soon turn up."
+29
+     
+30
+    elif fortuneRand == 3:
+31
+      print "\nYou will have bad luck for a week"
+32
+     
+33
+    elif fortuneRand == 4:
+34
+       print "\nToday is your lucky day!"
+35
+     
+36
+    elif fortuneRand ==5:
+37
+       print "\nYou will meet Mr. and Mrs. Right in 2 days!"
+38
+ if not the last cookie
+39
+    elif print"\n press any key to continue to your next fortune cookie."
+40
+     
+41
+    else print"\n This is your last cookie. Thank you for playing! Press any key to end the program"
+42
+     
+43
+ if fortuneRand 4
+44
+    else:
+45
+    print ("It's your lucky day! congratulations!!!")
+46
+     
+47
+    print("pick a number between 1 and 4 to claim your prize: ")
+48
+        n= input ()
+49
+        valn= int (n)
+50
+   if(number == '1'):
+51
+      print("\n\tCongratulations: You won the big prize! A free dinner up to a '\'4000 value! Thank you for playing!")
+52
+   elif(number == '2'):
+53
+       print("\n\tCongratulations: You won the big prize! A free appetizer '('Up to a '\'1500')'. Thank you for playing!")
+54
+   elif(number == '3'):
+55
+       print("\n\tCongratulations: You won the big prize! Two free bubble tea with dinner. Thank you for playing!")
+56
+   elif(number == '4'):   
+57
+        print("\n\tCongratulations: You won the big prize! 15 free fortune cookies. Thank you for playing!")
+58
+   else:
+59
+      print ("\n\tYou have not made a valid selection")
+60
+       
+61
+   input("\n\nPress Enter to exit the program:)
+62
+    
+63
+main()
